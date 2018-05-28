8d30d94e28b628d8b201d50db9e31da549144e71
Codes97 <santi_12_97@hotmail.com>
2018-05-27 21:17:34 -0300

    Added web

--- src/main/java/rest/QueryEndPoint.java
+++ src/main/java/rest/QueryEndPoint.java
@@ -4,10 +4,7 @@ import services.QueryService;
 import entityClasses.Document;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 @Path("/search")
@@ -18,8 +15,8 @@ public class QueryEndPoint {
     @GET
     @Path("{string}")
     @Produces("application/json")
-    public Response getQuery(@PathParam("string") String s) {
-        if (q.search(s)) {
+    public Response getQuery(@PathParam("string") String Nombre) {
+        if (q.search(Nombre)) {
             Document[] d = q.getSortedDocuments();
             return Response.ok(d).build();
         }
